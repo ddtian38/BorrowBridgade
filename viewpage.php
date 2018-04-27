f339c274ba424833c268f49c7943ac7f05a40be7
wangweiyi722 <wangweiyi722@gmail.com>
2018-04-26 20:41:18 -0500

    fixed the viewpage.php. Should be completely functional now

--- viewpage.php
+++ viewpage.php
@@ -1,4 +1,5 @@
 <?php
+
 if(isset($_POST['viewall'])){
 unset($_POST['submit']);
 
@@ -28,28 +29,27 @@ $result = mysqli_query($connect,"SELECT * from items ORDER BY original_location,
 print <<<TABLE_START
   <table border="1">
     <tr>
-      <th>ID</th><th>ITEM</th><th>CATEGORY</th><th>QUANTITY</th><th>Current Location</th><th>Current Possessor</th><th>Original Location</th><th>Original Possessor</th>
+      <th>ID</th><th>ITEM</th><th>CATEGORY</th><th>QUANTITY</th><th>CURRENT LOCATION</th><th>CURRENT POSSESSOR</th><th>ORIGINAL LOCATION</th><th>ORIGINAL POSSESSOR</th>
     <tr>
 TABLE_START;
 while($row = $result->fetch_row())
 {
+  
   print <<<TABLE_CONTENT
     <tr>
       <td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td><td>$row[7]</td>
     </tr>
 TABLE_CONTENT;
-  //print "ID = ".$row[0]."LastName = " . $row[1] . " FirstName = " . $row[2].
-//	" Major = " . $row[3] . " GPA = " . $row[4] . "<br /><br />\n";
 }
 print <<<TABLE_END
   </table>
 TABLE_END;
 
 $result->free();
-
+//unset($_POST['viewall']);
 mysqli_close($connect);
 }
-/*
+
 if(isset($_POST['submit'])){
 unset($_POST['viewall']);
 extract($_POST);
@@ -85,109 +85,97 @@ $table = "items";
 
 // If there is an id given, use that as the search parameter.
 
-
 if($id != ""){
-  $result = mysqli_query($connect,"SELECT * from $table WHERE id=$id");
+  echo "id option";
+  $result = mysqli_query($connect,"SELECT * from $table WHERE item_id=$id");
 }
+
+elseif($id == "" && $name == "" && $category == "" && $curr_location == "" && $curr_possessor == "" && $orig_location == "" && $orig_possessor == ""){
+  echo "no fields option";
+  $result = mysqli_query($connect,"SELECT * from $table");
+}
+
 else {
-  
-  $querystart = "SELECT * FROM $table";
+  echo "some fields option";
+  $querystart = "SELECT * FROM $table ";
   $whereclause = "WHERE ";
   $first = true;
+
   if($name!=""){
+    
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."name=\'$name\' ";
+    $whereclause = $whereclause."name='$name' ";
     $first = false;
   }
+
   if($category!=""){
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."category=\'$category\' ";
+    $whereclause = $whereclause."category='$category' ";
     $first = false;
   }
   if($curr_location!=""){
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."current_location=\'$curr_location\' ";
+    $whereclause = $whereclause."current_location='$curr_location' ";
     $first = false;
   }
   if($curr_possessor!=""){
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."current_possessor=\'$curr_possessor\' ";
+    $whereclause = $whereclause."current_possessor='$curr_possessor' ";
     $first = false;
   }
   if($orig_location!=""){
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."original_location=\'$orig_location\' ";
+    $whereclause = $whereclause."original_location='$orig_location' ";
     $first = false;
   }
   if($orig_possessor!=""){
     if(!$first){
-      $whereclause = $whereclause."AND "
+      $whereclause = $whereclause."AND ";
     }
-    $whereclause = $whereclause."original_possessor=\'$orig_possessor\' ";
+    $whereclause = $whereclause."original_possessor='$orig_possessor' ";
     $first = false;
   }
-  
+
+  echo $querystart.$whereclause;
+  $result = mysqli_query($connect,"$querystart$whereclause");
+
 }
-/*
-// If both a first and last name are given, return all rows with people who have both that first and last name
-elseif($first!="" and $last!=""){
-  $result = mysqli_query($connect,"SELECT * from $table WHERE FIRST='$first' and LAST='$last'");
-}
-
-// If only a first name or only a last name is given
-
-elseif($first!=""){
-  $result = mysqli_query($connect,"SELECT * from $table WHERE First='$first'");
-}
-
-
-elseif($last!=""){
-  $result = mysqli_query($connect,"SELECT * from $table WHERE Last='$last'");
-}
-
-// If nothing is defined, just display entire table
-
-else{
-  $result = mysqli_query($connect,"SELECT * from $table ORDER BY Last,First");
-}
-
-// Display the results table
+//echo $result;
 
 print <<<TABLE_START
   <table border="1">
     <tr>
-      <th>ID</th><th>LAST</th><th>FIRST</th><th>MAJOR</th><th>GPA</th>
+      <th>ID</th><th>ITEM</th><th>CATEGORY</th><th>QUANTITY</th><th>CURRENT LOCATION</th><th>CURRENT POSSESSOR</th><th>ORIGINAL LOCATION</th><th>ORIGINAL POSSESSOR</th>
     <tr>
 TABLE_START;
 while($row = $result->fetch_row())
 {
   print <<<TABLE_CONTENT
     <tr>
-      <td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td>
+      <td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td><td>$row[7]</td>
     </tr>
 TABLE_CONTENT;
-  //print "ID = ".$row[0]."LastName = " . $row[1] . " FirstName = " . $row[2].
-//	" Major = " . $row[3] . " GPA = " . $row[4] . "<br /><br />\n";
 }
 print <<<TABLE_END
   </table>
 TABLE_END;
 
+
 $result->free();
 
 mysqli_close($connect);
+
 }
-*/
 ?>
 
 <!DOCTYPE html>
@@ -224,6 +212,7 @@ mysqli_close($connect);
   <br>
   Current Possessor:<br>
   <select name="curr_possessor">
+    <option value=""></option>
 <?php
 
 // Connect to the MySQL database
@@ -256,12 +245,13 @@ echo "<option value=\"$userid\">" . $row['last_name'] . ", " . $row['first_name'
 }
 mysqli_close($connect);
 ?>
-  </select>
+  </select><br>
   Original Location:<br>
   <input type="text" name="orig_location">
   <br>
   Original Possessor:<br>
   <select name="orig_possessor">
+    <option value=""></option>
 <?php
 
 // Connect to the MySQL database
